836ca225f18eb1d9fdcbb2c9b8736d20dcf64b65
ThanThanhThuan <sangxuan1406@gmail.com>
2026-01-26 21:10:11 +0700

    Fix vite config import path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/vue-plugin'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
