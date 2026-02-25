1107c3da1d1799e4e8dcf3e31eb0c0f2416fc306
ThanThanhThuan <sangxuan1406@gmail.com>
2026-02-25 16:36:54 +0700

    ro

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import ServicesView from '../views/ServicesView.vue'
@@ -10,7 +10,7 @@ import MessagesView from '../views/MessagesView.vue'
 import TestimonialsView2 from '../views/TestimonialsView2.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         { path: '/', name: 'home', component: HomeView },
         { path: '/about', name: 'about', component: AboutView },
